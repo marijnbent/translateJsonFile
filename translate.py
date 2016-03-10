f26182e883ba785cae017b792f3617ddd5b779ef
marijnbent <marijnbent@gmail.com>
2016-03-10 23:33:55 +0100

    Making the files user-friendly

--- translate.py
+++ translate.py
@@ -1,15 +1,20 @@
 import json
 from textblob import TextBlob
 
-with open('language.json') as data_file:
-    data = json.load(data_file)
+#Set your language as toLanguage. Language codes: https://cloud.google.com/translate/v2/using_rest#language-params
+toLanguage = "nl"
+#Optional, use a different file
+file_name = "language.json"
+#And thats all! Run the code and enjoy. Be sure the check the translated file, Google Translate is not perfect.
 
 translatedDict = {}
 translatedString = ""
-toLanguage = "nl"
+
+with open(file_name) as data_file:
+    data = json.load(data_file)
 
 for key,value in data.items():
-    print ("EN:" + key)
+    print ("en:" + key)
     b = TextBlob(key)
 
     try:
@@ -19,10 +24,12 @@ for key,value in data.items():
         translatedString = key
         pass
 
-    print (toLanguage.capitalize() + ":" + translatedString)
+    print (toLanguage + ":" + translatedString)
     translatedDict[key] = translatedString
 
 translatedDict['LANG_NAME'] = toLanguage
+
+print("Translating succeeded. Be sure to check the '" + toLanguage + ".json' and  improve the translation.")
 
 with open(toLanguage + '.json', 'w') as result:
     json.dump(translatedDict, result)
